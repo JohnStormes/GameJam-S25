fe5cb6d05e0d5356bbf43a9965485cd92beb8a21
Johnathan Stormes <johnstormesjgn@gmail.com>
2025-04-12 02:20:51 -0400

    clock, sky animations

---
+++ Assets/Scripts/SkyMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyMovement : MonoBehaviour
+{
+    int START_POS = -1450;
+    int END_POS = 1450;
+    [SerializeField] GameObject sky2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetComponent<RectTransform>().anchoredPosition = new Vector3(START_POS, 250, 0);
+        sky2.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, 250, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move the images
+        transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y, 0);
+        sky2.transform.position = new Vector3(sky2.transform.position.x + 0.01f, sky2.transform.position.y, 0);
+
+        // reset an image if it goes too far
+        if (transform.GetComponent<RectTransform>().anchoredPosition.x > END_POS)
+            transform.GetComponent<RectTransform>().anchoredPosition = new Vector3(START_POS, 250, 0);
+        if (sky2.GetComponent<RectTransform>().anchoredPosition.x > END_POS)
+            sky2.GetComponent<RectTransform>().anchoredPosition = new Vector3(START_POS, 200, 0);
+    }
+}
