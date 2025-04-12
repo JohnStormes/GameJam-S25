3adbbe393f33f03be82de3940a47cf9cd651d0fd
Johnathan Stormes <johnstormesjgn@gmail.com>
2025-04-12 17:51:42 -0400

    tail wag

---
+++ Assets/Scripts/car.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour
+{
+    private Animator animator;
+    int random;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(GenerateRandom());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GenerateRandom() {
+        while (true) {
+            random = Random.Range(0, 4);
+            if (random == 1)
+                animator.SetTrigger("wag");
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
