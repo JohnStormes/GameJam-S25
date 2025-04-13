9ded659feb106ba333e0370b9753a65f7d94d0d2
Johnathan Stormes <johnstormesjgn@gmail.com>
2025-04-12 20:12:08 -0400

    run out da do

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class LoadScene : MonoBehaviour
 {
+    private Animator cat_animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void StartCat() {
+        cat_animator = GameObject.Find("Cat").GetComponent<Animator>();
+        cat_animator.SetTrigger("cat_dialogue");
     }
 }
